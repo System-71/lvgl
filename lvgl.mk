24f786c414a5f9d645c0c5252ddae7e17b75d35f
Mitchell Johnston <mitchelljohnston@hotmail.ca>
2024-07-07 09:44:08 -0700

    modify directory path in Makefile for our build process

--- lvgl.mk
+++ lvgl.mk
@@ -1,4 +1,4 @@
-LVGL_PATH ?= ${shell pwd}/lvgl
+LVGL_PATH ?= ${shell pwd}/lvgl_
 
 CSRCS += $(shell find $(LVGL_PATH)/src -type f -name '*.c')
 CSRCS += $(shell find $(LVGL_PATH)/demos -type f -name '*.c')
